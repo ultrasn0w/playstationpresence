5c0a943cde68d26f92f4eed8512bbe9e8b1800f4
ultrasn0w <114.barth@gmail.com>
2022-01-06 17:01:55 +0100

    Modify discord texts &  prepare large_icon logic

--- playstationpresence/playstationpresence.py
+++ playstationpresence/playstationpresence.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import asyncio
+import time
 from psnawp_api import psnawp
 from pypresence import Presence
 from playstationpresence.lib.files import load_config, load_game_data
@@ -8,12 +9,13 @@ from playstationpresence.lib.rpc_retry import rpc_retry
 from requests.exceptions import *
 from threading import Event
 
+
 class PlaystationPresence:
     def __init__(self):
         self.notifier = None
         self.rpc = None
         self.exit_event = Event()
-        self.old_info: dict = { 'onlineStatus': None, 'titleId': None }
+        self.old_info: dict = {'onlineStatus': None, 'titleId': None}
         self.config: dict = load_config()
         self.supported_games: set[str] = load_game_data()
         self.psapi = psnawp.PSNAWP(self.config['npsso'])
@@ -30,7 +32,7 @@ class PlaystationPresence:
         if self.notifier is not None:
             self.notifier.visible = False
             self.notifier.stop()
-    
+
     def notify(self, message):
         print(message)
 
@@ -44,30 +46,47 @@ class PlaystationPresence:
         self.notify(f"Status changed to Offline")
 
     @rpc_retry
-    def updateStatus(self, state: str, large_image: str, large_text: str, tray_tooltip: str):
-        self.rpc.update(state=state, small_image="ps5_main", small_text=self.psnid, large_image=large_image, large_text=large_text)
-        self.notify(f"Status changed to {tray_tooltip}")
+    def updateStatus(self, show_time: bool, state: str, large_image: str, details: str):
+        if show_time:
+            start_time = int(time.time())
+            self.rpc.update(state=state, start=start_time, small_image="ps5_main", small_text=self.psnid, large_image=large_image, large_text=state, details=details)
+        else:
+            self.rpc.update(state=state, small_image="ps5_main", small_text=self.psnid, large_image=large_image, large_text=state, details=details)
+
+        self.notify(f"Status changed to {state}")
 
     def processPresenceInfo(self, mainpresence: dict):
         if mainpresence is None:
             return
 
+        # Read PSN API data
         onlineStatus: str = mainpresence['primaryPlatformInfo']['onlineStatus']
+        onlinePlatform: str = mainpresence['primaryPlatformInfo']['platform']
         game_info: list[dict] = mainpresence.get('gameTitleInfoList', None)
-        
+
+        # Check online status
         if onlineStatus == "offline":
             if self.old_info['onlineStatus'] != onlineStatus:
                 self.clearStatus()
-                self.old_info = { 'onlineStatus': onlineStatus, 'titleId': None }
+                self.old_info = {'onlineStatus': onlineStatus, 'titleId': None}
         elif game_info == None:
+            # Set home menu state
             if self.old_info['onlineStatus'] != "online" or self.old_info['titleId'] != None:
-                self.updateStatus("Not in game", "ps5_main", "Homescreen", "Not in game")
-                self.old_info = { 'onlineStatus': onlineStatus, 'titleId': None }
+                self.updateStatus(False, "Home Menu", "ps5_main", f"Online on {onlinePlatform}")
+                self.old_info = {'onlineStatus': onlineStatus, 'titleId': None}
         elif self.old_info['titleId'] != game_info[0]['npTitleId']:
+            # New title id is different -> update
+            # Read game data
             game: dict[str, str] = game_info[0]
-            large_icon = game['npTitleId'].lower() if game['npTitleId'] in self.supported_games else "ps5_main"
-            self.updateStatus(game['titleName'], large_icon, game['titleName'], f"Playing {game['titleName']}")
-            self.old_info = { 'onlineStatus': onlineStatus, 'titleId': game['npTitleId'] }
+            # large_icon logic
+            if game['npTitleId'] in self.supported_games:
+                large_icon = game['npTitleId'].lower()
+            else:
+                large_icon = "ps5_main"
+            # Update status
+            self.updateStatus(
+                True, game['titleName'], large_icon, f"Playing on {game['launchPlatform']}")
+            self.old_info = {'onlineStatus': onlineStatus, 'titleId': game['npTitleId']}
 
     def mainloop(self, notifier: Notifiable):
         if notifier is not None:
@@ -82,13 +101,14 @@ class PlaystationPresence:
                 user_online_id = self.psapi.user(online_id=self.psnid)
                 mainpresence = user_online_id.get_presence()
                 # Uncomment for debug info about currently running game
-                #print(mainpresence)
+                # print(mainpresence)
             except (ConnectionError, HTTPError) as e:
                 print("Error when trying to read presence")
                 print(e)
 
             self.processPresenceInfo(mainpresence)
-            self.exit_event.wait(20) #Adjust this to be higher if you get ratelimited
+            # Adjust this to be higher if you get ratelimited
+            self.exit_event.wait(20)
 
         self.clearStatus()
         self.rpc.close()
